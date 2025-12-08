fa157f5128bbc1f11da829c73ac8cb6aa53e8edf
PPROTECH <PPROTECH@global.corp.sap>
2025-12-08 12:57:14 +0000

    samples & runtimes for apl 4.2602.0

--- sql/check_apl/check_apl.sql
+++ sql/check_apl/check_apl.sql
@@ -531,6 +531,8 @@ BEGIN
 		:known_versions.insert(('2516','172','193'));
 		:known_versions.insert(('2520','172','193'));
 		:known_versions.insert(('2522','172','193'));
+		:known_versions.insert(('2524','172','193'));
+		:known_versions.insert(('2602','172','193'));
 		-- we search if the current version is a well known version
 		SELECT "VERSION","TABLES_TYPES","DU_APIS" into ref_version,ref_nb_apl_tables_types,ref_nb_apl_dus DEFAULT 'NotFound',-1,-1 FROM :known_versions WHERE "VERSION" = :du_version;
 		IF :ref_version = 'NotFound'
